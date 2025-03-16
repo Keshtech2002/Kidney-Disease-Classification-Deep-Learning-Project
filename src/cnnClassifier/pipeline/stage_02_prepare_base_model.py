278f287ad78d13fa465f70bc435327244c6c567d
Keshtech2002 <keshtech2002@gmail.com>
2025-03-16 12:43:17 +0100

    edit

--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -20,7 +20,8 @@ class PrepareBaseModelTrainingPipeline:
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
+        # obj = DataIngestionTrainingPipeline()
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
